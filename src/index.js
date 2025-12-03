afc7bb5fa9c143408ca9f940f57021dace70bb11
Adnan00707 <adnansoni2020@gmail.com>
2025-12-04 00:17:06 +0400

    Updated routing and app pages

--- src/index.js
+++ src/index.js
@@ -5,19 +5,17 @@ import App from "./App";
 import Landing from "./Landing";
 import Login from "./Login";
 import ProductDetails from "./ProductDetails";
-import Checkout from "./Checkout";
 
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <Router>
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/login" element={<Login />} />
       <Route path="/app" element={<App />} />
       <Route path="/product/:id" element={<ProductDetails />} />
-      <Route path="/checkout" element={<Checkout />} />
     </Routes>
-  </HashRouter>
+  </Router>
 );
