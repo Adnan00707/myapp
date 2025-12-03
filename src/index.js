f526e163b85eed944ddddb96f6619eb45ea644c9
Adnan00707 <adnansoni2020@gmail.com>
2025-12-04 00:21:30 +0400

    Fix routing for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import Landing from "./Landing";
 import Login from "./Login";
 import ProductDetails from "./ProductDetails";
 
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/login" element={<Login />} />
       <Route path="/app" element={<App />} />
       <Route path="/product/:id" element={<ProductDetails />} />
     </Routes>
-  </Router>
+  </HashRouter>
 );
