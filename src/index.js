f44f5604b7da3e0e2a1b521fc09fc9dfd26f4799
Adnan00707 <adnansoni2020@gmail.com>
2025-12-04 00:04:59 +0400

    Updated project with working landing, login, routing

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import App from "./App";
 import Landing from "./Landing";
 import Login from "./Login";
-import App from "./App";
-import ProductDetails from "./ProductDetails";
-import Checkout from "./Checkout";
 
-import { CartProvider } from "./CartContext";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <CartProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/app" element={<App />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
-    </BrowserRouter>
-  </CartProvider>
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/app" element={<App />} />
+    </Routes>
+  </HashRouter>
 );
